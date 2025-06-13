0e45c76579f545740320c693a38a8247b40f98f0
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-13 23:15:52 +0200

    iniziato Hero

--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Hero Vue component</div>
+  <section
+    id="home"
+    class="w-full flex xl:flex-row justify-center min-h-screen gap-10 max-container"
+  >
+    <div
+      class="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:paddomg-x pt-28"
+    >
+      <p>Our Summer Collection</p>
+      <h1>
+        <span>The New Arrival</span><br />
+        <span>Nike</span>
+        Shoes
+      </h1>
+      <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+    </div>
+  </section>
 </template>
