6143404e9bc1c71ac37deb45278cf6c8332bf58b
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-14 00:00:52 +0200

    Hero fino al button

---
+++ src/components/NikeButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+    class="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white transition-colors duration-300 border rounded-full px-7 font-montserrat bg-coral-red border-coral-red"
+  >
+    {{ label }}
+    <img class="w-5 h-5 ml-12 rounded-full" v-if="iconURL" :src="iconURL" alt="arrow right icon" />
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: 'Click Me',
+  },
+  iconURL: {
+    type: String,
+    default: '',
+  },
+})
+</script>
--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -1,10 +1,15 @@
+<script setup>
+import NikeButton from '../NikeButton.vue'
+import { arrowRight } from '@/assets/icons'
+</script>
+
 <template>
   <section
     id="home"
-    class="w-full flex xl:flex-row justify-center min-h-screen gap-10 max-container"
+    class="flex justify-center w-full min-h-screen gap-10 xl:flex-row max-container"
   >
     <div
-      class="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:paddomg-x pt-28"
+      class="relative flex flex-col items-start justify-center w-full xl:w-2/5 max-xl:padding-x pt-28"
     >
       <p>Our Summer Collection</p>
       <h1>
@@ -13,6 +18,7 @@
         Shoes
       </h1>
       <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+      <NikeButton :iconURL="arrowRight" label="Shop Now" />
     </div>
   </section>
 </template>
