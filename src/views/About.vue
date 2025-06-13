c553cac0de5f34458ab50e9fd9885076749a3fff
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2025-06-13 14:53:59 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="bg-[url('https://picsum.photos/1920/1080')] bg-no-repeat">
     <div class="flex h-screen justify-center items-center">
-      <h1 class="text-9xl">ABOUT</h1>
+      <h1 class="text-9xl">ABOUT Davide </h1>
     </div>
   </section>
 </template>
