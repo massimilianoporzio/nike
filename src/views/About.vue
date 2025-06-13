d57d5401db35e7a4290d0d833c4433ef40529c9e
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-13 14:48:56 +0200

    prova di navigazione spa

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="bg-[url('https://picsum.photos/1920/1080')] bg-no-repeat">
+    <div class="flex h-screen justify-center items-center">
+      <h1 class="text-9xl">ABOUT</h1>
+    </div>
+  </section>
+</template>
