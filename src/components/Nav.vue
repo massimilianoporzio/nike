9335f8950884c9cda87506825e5e27a7fd7c7028
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-13 22:48:10 +0200

    open close navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -30,10 +30,34 @@ var isOpen = ref(false)
         <a href="/">Explore now</a>
       </div>
       <div class="max-lg:flex flex-col items-center justify-center hidden">
-        <img :src="hamburger" alt="Hamburger Menu" width="25" height="25" />
-        <div class="bg-sky-50 w-50 h-full mt-2">
-          <ul class="text-center my-3">
-            <li v-for="link in navLinks" :key="link.label" class="mb-3">
+        <img
+          class="self-start cursor-pointer"
+          :src="hamburger"
+          alt="Hamburger Menu"
+          width="25px"
+          height="25px"
+          v-if="!isOpen"
+          @click="isOpen = !isOpen"
+        />
+        <svg
+          @click="isOpen = !isOpen"
+          preserveAspectRatio="xMidYMid meet"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 22 22"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-7 self-end cursor-pointer"
+          v-if="isOpen"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+        <div
+          class="bg-white--400 w-50 h-50 mt-2 flex flex-col justify-center items-center"
+          v-if="isOpen"
+        >
+          <ul class="flex flex-col gap-2 justify-around items-center">
+            <li v-for="link in navLinks" :key="link.label">
               <a
                 href="#"
                 v-scroll-to="link.href"
@@ -43,6 +67,13 @@ var isOpen = ref(false)
               </a>
             </li>
           </ul>
+          <div
+            className="flex mt-4 gap-2 text-lg leading-normal font-medium font-montserrat max-lg:block wide:mr-24"
+          >
+            <a href="/">Sign in</a>
+            <span>/</span>
+            <a href="/">Explore now</a>
+          </div>
         </div>
       </div>
     </nav>
