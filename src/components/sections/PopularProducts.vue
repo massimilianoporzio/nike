32b4940d4793c0b70837a9423145f935179ea1ff
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-16 16:56:09 +0200

    da ufficio

--- src/components/sections/PopularProducts.vue
+++ src/components/sections/PopularProducts.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { products } from '@/constants'
+</script>
 <template>
-  <div>Popular Products Vue component</div>
+  <section id="products" class="max-container max-sm:mt-12">
+    <div class="flex flex-col justify-start gap-5">
+      <h2 class="text-4xl font-palanquin font-bold">
+        Our <span class="text-coral-red">Popular</span> Products
+      </h2>
+      <p class="lg_max-w-lg mt-2 font-montserrat text-slate-gray">
+        Experiece top-notch quality and style with our sought-after selections. Discover a world of
+        comfort, design and value
+      </p>
+    </div>
+  </section>
 </template>
